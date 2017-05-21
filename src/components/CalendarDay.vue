8317c9732c0e50b23ece9ad95a7fd2cf667f771a
Craig Palermo <craig.palermo@protonmail.com>
2017-05-20 21:18:34 -0400

    Implement sum for month

---
+++ src/components/CalendarDay.vue
@@ -0,0 +1,20 @@
+<template>
+  <input type="number"
+         min="0"
+         v-on:input="valueChanged">
+</template>
+
+<script>
+  export default {
+    props: ['index', 'days'],
+    name: 'calendar-day',
+    methods: {
+      valueChanged(event) {
+        this.$emit('valueChanged', {
+          index: this.index,
+          value: event.target.valueAsNumber,
+        });
+      },
+    },
+  };
+</script>
--- src/components/CalendarMonth.vue
+++ src/components/CalendarMonth.vue
@@ -1,46 +1,72 @@
 <template>
   <div class="month">
+    <!-- month name -->
     <h1>{{ monthName }}</h1>
 
+    <!-- calendar -->
     <div class="days-container">
       <div class="day"
-           v-for="day in days">
+           v-for="(day, idx) in days">
         <div>
-          <div type="number" v-if="day.active">{{ day.value }}</div>
+          <div v-show="day.active">
+            <calendar-day :index="idx" v-on:valueChanged="updateSum"></calendar-day>
+          </div>
         </div>
       </div>
+    </div>
+
+    <!-- totals -->
+    <div>
+      {{ sum }}
     </div>
   </div>
 </template>
 
 <script>
   import moment from 'moment';
+  import CalendarDay from './CalendarDay';
 
   export default {
     name: 'calendar-month',
     props: ['number'],
+    components: {
+      CalendarDay,
+    },
     data() {
       return {
         days: this.getValuesArray(this.number),
+        sum: 0,
       };
     },
     methods: {
+      initValuesArray(num, active) {
+        let i = 0;
+        const values = [];
+        while (i < num) {
+          values.push({
+            active,
+            value: 0,
+          });
+          i += 1;
+        }
+        return values;
+      },
       getValuesArray(number) {
         const firstOfMonth = moment(new Date(moment().year(), number - 1)).date(1);
         const daysInMonth = firstOfMonth.daysInMonth();
         const firstWeekdayOffset = firstOfMonth.isoWeekday();
 
         // Placeholder values for end of previous month
-        const inactiveValues = new Array(firstWeekdayOffset).fill({
-          active: false,
-        });
+        const inactiveValues = this.initValuesArray(firstWeekdayOffset, false);
 
         // Active values, one for each day of month
-        const activeValues = new Array(daysInMonth).fill({
-          active: true,
-          value: 0,
-        });
+        const activeValues = this.initValuesArray(daysInMonth, true);
+
         return [...inactiveValues, ...activeValues];
+      },
+      updateSum(data) {
+        this.days[data.index].value = data.value;
+        this.sum = this.days.reduce((total, day) => total + day.value, 0);
       },
     },
     computed: {
@@ -64,5 +90,9 @@
 
   .day {
     width: (1 / 7) * 100%;
+  }
+
+  input {
+    width: 100%;
   }
 </style>
